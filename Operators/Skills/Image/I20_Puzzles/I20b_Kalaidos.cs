7d578fde94db82ffc974993797bacf3cbdd76894
Thomas Mann <thomas.mann@framefield.com>
2026-01-02 15:55:09 +0100

    SkillMap: Fixing namespaces

--- Operators/Skills/Image/I20_Puzzles/I20b_Kalaidos.cs
+++ Operators/Skills/Image/I20_Puzzles/I20b_Kalaidos.cs
@@ -1,7 +1,7 @@
-namespace Skills.Image.I01_BasicShapes;
+namespace Skills.Image.I20_Puzzles;
 
 [Guid("98046598-4b25-44d3-95ef-268226a09038")]
-internal sealed class I01c_Kalaidos : Instance<I01c_Kalaidos>
+internal sealed class I20b_Kalaidos : Instance<I20b_Kalaidos>
 {
     [Output(Guid = "1cc22e61-00a6-4852-aa58-8bec78140790")]
     public readonly Slot<Texture2D> ColorBuffer = new();
